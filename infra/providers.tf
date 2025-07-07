e5bd76bb1287f5f39530ef27bc48670a53e49185
Adam Webb <adamwebb@microsoft.com>
2025-07-07 19:24:56 +0000

    refactor: Update Terraform backend configuration to use 'azurerm' instead of 'local'

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,7 +1,12 @@
 terraform {
   required_version = ">= 1.9.0"
 
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = var.terraform_state_rg
+    storage_account_name = var.terraform_state_storage
+    container_name       = var.terraform_state_container
+    key                  = "${var.environment}.tfstate"
+  }
 
   required_providers {
     azurerm = {
